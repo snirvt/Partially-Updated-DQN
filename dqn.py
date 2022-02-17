0138298e9ac3eddb4c2c8d9e5ba4768216cde5d9
snirvit <snirvt@gmail.com>
2022-02-17 21:38:52 +0200

    added transfer learning

--- dqn.py
+++ dqn.py
@@ -3,6 +3,47 @@ import numpy as np
 import torch
 from torch import nn
 
+class DQN_2_layers(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DQN_2_layers, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
+
+    def transfer_learning(self, weights, freeze = True):
+        self.conv = weights.cuda()
+        if freeze:
+            for param in self.conv.parameters():
+                param.requires_grad = False
+
+
+
+
+
 class DQN(nn.Module):
     def __init__(self, input_shape, n_actions):
         super(DQN, self).__init__()
@@ -17,6 +58,7 @@ class DQN(nn.Module):
         )
 
         conv_out_size = self._get_conv_out(input_shape)
+
         self.fc = nn.Sequential(
             nn.Linear(conv_out_size, 512),
             nn.ReLU(),

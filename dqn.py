f54f664d9f2c1568f4f84e8614817e06dd0d188e
snirvit <snirvt@gmail.com>
2022-03-08 13:43:33 +0200

    changes in the DQN

--- dqn.py
+++ dqn.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import torch
-from torch import nn
+from torch import nn, sigmoid
 
 class DQN_2_layers(nn.Module):
     def __init__(self, input_shape, n_actions):
@@ -18,12 +18,23 @@ class DQN_2_layers(nn.Module):
 
         conv_out_size = self._get_conv_out(input_shape)
 
+        # self.fc = nn.Sequential(
+        #     nn.Linear(conv_out_size, 128),
+        #     nn.ReLU(),
+        #     nn.Linear(128, 128),
+        #     nn.ReLU(),
+        #     nn.Linear(128, n_actions)
+        # )
         self.fc = nn.Sequential(
-            nn.Linear(conv_out_size, 128),
+            nn.Linear(conv_out_size, 256),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(256, n_actions)
+        )
+        self.auto_encoder = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
             nn.ReLU(),
-            nn.Linear(128, n_actions)
+            nn.Linear(512, 84*84),
+            nn.Sigmoid()
         )
 
     def _get_conv_out(self, shape):
@@ -33,11 +44,16 @@ class DQN_2_layers(nn.Module):
     def forward(self, x):
         conv_out = self.conv(x).view(x.size()[0], -1)
         return self.fc(conv_out)
+    
+    def forward_ae(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.auto_encoder(conv_out)
 
     def transfer_learning(self, weights, freeze = True):
         self.conv = weights.cuda()
         if freeze:
             self.freeze()
+            
     def freeze(self):
         for param in self.conv.parameters():
             param.requires_grad = False
@@ -67,6 +83,7 @@ class DQN(nn.Module):
             nn.Linear(512, n_actions)
         )
 
+
     def _get_conv_out(self, shape):
         o = self.conv(torch.zeros(1, *shape))
         return int(np.prod(o.size()))
@@ -74,3 +91,4 @@ class DQN(nn.Module):
     def forward(self, x):
         conv_out = self.conv(x).view(x.size()[0], -1)
         return self.fc(conv_out)
+    

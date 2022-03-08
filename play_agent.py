883a1aac9411db4d885d44fc1ecb20703a2ca1c4
snirvt <snirvt@gmail.com>
2022-03-08 14:20:20 +0200

    play the game with saved weights

--- play_agent.py
+++ play_agent.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gym
 import torch
+import time
 
 from gym_wrappers import make_env
 from agent import Agent
@@ -10,53 +11,33 @@ from dqn import DQN, DQN_2_layers
 
 
 DEFAULT_ENV_NAME = "PongNoFrameskip-v4" 
+# DEFAULT_ENV_NAME = 'Breakout-v0'
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 render_img = True
-# print_vals = True
-
-# model = np.load("bestQ.npy", allow_pickle=True)
-
-
-
-
 
 env = make_env(DEFAULT_ENV_NAME)
 
-
-model = DQN(env.observation_space.shape, env.action_space.n)
-# model.load_state_dict(torch.load('PongNoFrameskip-v4-conv.dat',map_location=device))    
+model = DQN_2_layers(env.observation_space.shape, env.action_space.n)
+# model = DQN_2_layers(env.observation_space.shape, 6)
+model.load_state_dict(torch.load("models/trained_dqn.dat",map_location=device))    
 
 
 buffer = ExperienceReplay(capacity=0)
 agent = Agent(env, buffer)
 
-cnt = 0
-# observation = env.reset()
 if render_img:
     _ = env.render(mode="human")
 
-total_reward = 0
 print_list = []
-for num_step in range(200):
-    cnt+=1
-    # f_s = get_features(observation)
-    # action = get_mean_action(f_s, bestQ)
-    agent.play_step(model)
-    # observation, reward, done, info = env.step(action)
-    # reward += 20
+for num_step in range(2000):
+    reward = agent.play_step(model, epsilon=0)
     if render_img:
         _ = env.render(mode="human")
-    # if print_vals:
-        # print_list.append([cnt, *action[0], observation, reward[0]])
-    # total_reward += reward
-    # if done:
-    #     observation = env.reset()
-    #     break
+        time.sleep(0.02)
+    if reward is not None:
+        break
 env.close()
-
-# print('Games over\nNumber of steps: {}\nTotal reward: {}\n'.format(num_step+1, total_reward))
-
 
 
